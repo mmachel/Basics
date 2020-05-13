ac152f020a6aa0de451317058d2d570eb0facfb2
MMachel <63281902+MMachel@users.noreply.github.com>
2020-05-13 19:59:48 +0200

    Add py fie

---
+++ additon.py
@@ -0,0 +1,13 @@
+#Simple addition
+x = int(input("Enter number1: "))
+y= int(input("Enter number2: "))
+z = x + y
+print(f"The sum is now {z}")
+
+#Addition using function
+def add():
+	x = 43
+	y = 17
+	print("Sum is "+str(x+y))
+
+add()
